f4e58b7abc9db77ee312e5a85d8960f6ca6a2965
Robert <mccannb401@gmail.com>
2021-04-21 22:32:15 +0100

    Subscriber Project - CRUD and RESTful API project complete

---
+++ README.md
@@ -0,0 +1,15 @@
+#####
+**Subscriber Project**
+#####
+
+I completed this by going through the amazing tutorial by Web Dev Simplified.
+
+[Build A REST API With Node.js, Express, & MongoDB - Quick](https://www.youtube.com/watch?v=fgTGADljAeg) 
+
+I could not get .env to work with this tutorial though, hopefully I can look back on this in the future and figure out why :) 
+
+#
+It uses:
+
+Mongoose, Express and Nodemon (dotenv too, but as mentioned above not currently working)
+#
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -16,8 +16,8 @@ router.get('/', async (req, res) => {
 
 //get one
 
-router.get('/:id',(req, res)=> {
-    res.send(req.params.id)
+router.get('/:id', getSubscriber, (req, res)=> {
+    res.json(res.subscriber)
 
 })
 //create one
@@ -38,13 +38,52 @@ router.post('/', async (req,res) => {
 
 })
 //update one
-router.patch('/', (req,res) => {
+router.patch('/:id', getSubscriber, async (req,res) => {
+    if (req.body.name != null) {
+        res.subscriber.name = req.body.name
+    }
+    if (req.body.subscribedToChannel != null) {
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+
+    } catch (err) {
+        res.status(400).json({message: err.message})
+
+    }
 
 })
 //deleting one
 
-router.delete('/:id', (req,res) => {
+router.delete('/:id', getSubscriber, async (req,res) => {
+
+    try {
+        await res.subscriber.remove()
+        res.json({ message: 'Deleted Subscriber'})
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+
+    }
 
 })
+
+async function getSubscriber(req, res, next) {
+    let subscriber
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber'})
+        }
+
+    } catch(err){
+        return res.status(500).json({message: err.message})
+
+    }
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
